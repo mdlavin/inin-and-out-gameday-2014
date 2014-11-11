f558fb7af59a3226bae7f75e627563bfb83598c4
Matt Lavin <matt.lavin@inin.com>
2014-11-11 12:46:17 -0800

    String together create and launch-nat

--- create.sh
+++ create.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-aws ec2 create-vpc --cidr-block 10.0.1.0/24
+VPC_ID=`aws ec2 create-vpc --cidr-block 10.0.1.0/24 | grep VpcId | grep -o vpc-[a-z0-9]*`
+
+echo "Created VPC $VPC_ID"
+./launch-nat.sh $VPC_ID
