ed577fb74f8776a46dfe0e3582eacdb790182656
Chris Hemp <chris@chrishemp.com>
2014-11-11 12:11:00 -0800

    Add keynote

--- create-master-ec2-instance.sh
+++ create-master-ec2-instance.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-VPC_ID=$1
+SG_IDS=$1
+SUBNET_ID=$2
 
 # Amazon Linux AMI 2014.09.1 (HVM) - ami-4985b048
 AMI_ID=ami-e74b60e6
@@ -9,7 +10,8 @@ SNAPSHOT_ID=snap-025e7c85
 
 # Create EC2 Instance
 # TODO: Switch role to BatchProcessing
-aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t2.micro --iam-instance-profile Name=hemp --user-data batch-processing-user-data.sh --block-device-mappings "[{\"DeviceName\":\"/dev/xvda\",\"Ebs\":{\"DeleteOnTermination\":true,\"SnapshotId\":\"$SNAPSHOT_ID\",\"VolumeSize\":100,\"VolumeType\":\"standard\"}}]"
+# TODO: --security-group-ids $SG_IDS --subnet-id $SUBNET_ID
+aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t2.micro --iam-instance-profile Name=hemp --key-name gameday --user-data batch-processing-user-data.sh --block-device-mappings "[{\"DeviceName\":\"/dev/xvda\",\"Ebs\":{\"DeleteOnTermination\":true,\"SnapshotId\":\"$SNAPSHOT_ID\",\"VolumeSize\":100,\"VolumeType\":\"standard\"}}]"
 
 # Tag Instance
 # aws ec2 create-tags --resources i-xxxxxxxx Master --tag "name=Master"
