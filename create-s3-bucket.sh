e1ed9b7c25cd7afc5c897f26ca705f60b7783404
Matt Lavin <matt.lavin@inin.com>
2014-11-11 13:32:40 -0800

    Update create-s3-bucket so that it can be run shortly after a deletion

--- create-s3-bucket.sh
+++ create-s3-bucket.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws s3api create-bucket --bucket image-bucket-54232
+aws s3api create-bucket --bucket image-bucket-`uuidgen|tr '[:upper:]' '[:lower:]'` --create-bucket-configuration LocationConstraint=ap-northeast-1
