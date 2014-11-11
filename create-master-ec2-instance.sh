d7acefd6f88dd08a3c9c8b6b85b25809ed431c1f
Chris Hemp <chris@chrishemp.com>
2014-11-11 11:57:56 -0800

    Instance is alive!

--- create-master-ec2-instance.sh
+++ create-master-ec2-instance.sh
@@ -8,7 +8,8 @@ AMI_ID=ami-e74b60e6
 SNAPSHOT_ID=snap-025e7c85
 
 # Create EC2 Instance
-aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t2.micro --iam-profile BatchProcessing --user-data batch-processing-user-data.sh --block-device-mappings "[{\"DeviceName\":\"/dev/xvda\",\"Ebs\":{\"DeleteOnTermination\":true,\"SnapshotId\":\"$SNAPSHOT_ID\",\"VolumeSize\":100,\"VolumeType\":\"standard\"}}]"
+# TODO: Switch role to BatchProcessing
+aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t2.micro --iam-instance-profile Name=hemp --user-data batch-processing-user-data.sh --block-device-mappings "[{\"DeviceName\":\"/dev/xvda\",\"Ebs\":{\"DeleteOnTermination\":true,\"SnapshotId\":\"$SNAPSHOT_ID\",\"VolumeSize\":100,\"VolumeType\":\"standard\"}}]"
 
 # Tag Instance
 # aws ec2 create-tags --resources i-xxxxxxxx Master --tag "name=Master"
