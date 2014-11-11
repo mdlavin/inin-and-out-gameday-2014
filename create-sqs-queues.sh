82140f4c15b9f5c88b0e0207cd1f75b5b07ba4a8
Matt Lavin <matt.lavin@inin.com>
2014-11-11 13:21:49 -0800

    Allow Amazon access to insert messages

--- create-sqs-queues.sh
+++ create-sqs-queues.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-aws sqs create-queue --queue-name input --attributes VisibilityTimeout=90
+INPUT_QUEUE=`aws sqs create-queue --queue-name input --attributes VisibilityTimeout=90 | grep QueueUrl | grep -o 'http.*/input'`
+echo "Created input queue $INPUT_QUEUE"
+
 aws sqs create-queue --queue-name output --attributes VisibilityTimeout=90
+
+aws sqs add-permission --label 'Incoming messages from Amazon' --queue-url "$INPUT_QUEUE" --actions "SendMessage" --aws-account-ids 526039161745
 
